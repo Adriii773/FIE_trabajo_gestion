9ce95d2657366a31ca56e8a48336856fb9509e39
Adriii773 <adri.garfan05@gmail.com>
2026-04-08 18:05:17 +0200

    problemas al abrir el archivo

--- ddf/EDROOM_MODEL/UAHMarsDrone/ClasesActor/uahmarsdronee.cpp
+++ ddf/EDROOM_MODEL/UAHMarsDrone/ClasesActor/uahmarsdronee.cpp
@@ -19,6 +19,14 @@ UAHMarsDrone::UAHMarsDrone(TEDROOMComponentID id,
 		CEDROOMComponent(id,EDROOMprioMINIMUM+1,roomNumMaxMens,
 				roomtaskPrio,roomStack, pActorMemory ),
 
+		// *********** Timing service access point *********
+
+		EDROOMtimingSAP(this, 3,&pActorMemory->TimingMemory),
+
+		// *******************  Timers  ********************
+
+		Timer(&EDROOMtimingSAP, 2 ),
+
 		// ***************	Top State  *****************
 
 		edroomTopState(*this)
@@ -52,6 +60,10 @@ int UAHMarsDrone::EDROOMConfig()
 int UAHMarsDrone::EDROOMStart()
 {
 
+
+	//****************** Timing Task Start*****************
+
+	EDROOMtimingSAP.Start();
 
 	//***************** CEDROOMComponent::EDROOMStart*********
 
@@ -90,7 +102,7 @@ bool UAHMarsDrone::EDROOMIsComponentFinished()
 {
 
 
-	return ( DroneMng.EDROOMIsComponentFinished() && CEDROOMComponent::EDROOMIsComponentFinished());
+	return ( CEDROOMComponent::EDROOMIsComponentFinished());
 
 }
 
@@ -109,6 +121,8 @@ void UAHMarsDrone::CEDROOMMemory::SetMemory(TEDROOMUInt32 numMessages_ ,
 
 		CEDROOMComponentMemory::SetMemory( numMessages_,MessagesMem_, MessagesMemMarks_,
 			numberOfNodes_,QueueNodesMem_, QueueNodesMemMarks_, QueueHeads);
+
+		TimingMemory.SetMemory(3,TimerInf,&TimerInfMarks[0],TimeOutMsgs,&TimeOutMsgsMarks[0]);
 
 
 }
